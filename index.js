b46b337308b1a8a9a5e2c52a8cbebdd76ab903bc
Javier Suarez <javier@edlio.com>
2017-08-31 09:03:43 -0500

    adds country select

--- index.js
+++ index.js
@@ -1,14 +1,52 @@
 function initMap() {
 
+  var countries = [
+    {
+      "id": "us",
+      "position": new google.maps.LatLng(37.2583698, -104.6527086),
+      "name": "United States of America"
+    }, {
+      "id": "mx",
+      "position": new google.maps.LatLng(23.489557, -107.1130912),
+      "name": "México"
+    }, {
+      "id": "ca",
+      "position": new google.maps.LatLng(54.6997679, -113.7051108),
+      "name": "Canada"
+    }
+  ]
+
   var states = [
     {
       "id": "cdmx",
+      "country": "mx",
       "position": new google.maps.LatLng(19.4165072, -99.1234034),
       "name": "Ciudad de México"
     }, {
       "id": "edomex",
+      "country": "mx",
       "position": new google.maps.LatLng(19.2821786, -99.6808406),
       "name": "Estado de México"
+    }, {
+      "id": "california",
+      "country": "us",
+      "position": new google.maps.LatLng(33.9722659, -118.4651995),
+      "name": "California"
+    }, {
+      "id": "newyork",
+      "country": "us",
+      "position": new google.maps.LatLng(40.6974034,-74.1197629),
+      "name": "New York"
+    }, {
+      "id": "britishc",
+      "country": "ca",
+      "position": new google.maps.LatLng(49.2577143,-123.1939432),
+      "name": "British Columbia"
+    }, {
+      "id": "ontario",
+      "country": "ca",
+      "position": new google.maps.LatLng(43.6567919,-79.4609316),
+      "name": "Ontario"
     }
   ]
 
@@ -34,13 +72,41 @@ function initMap() {
       "position": new google.maps.LatLng(19.2776417, -99.6591684),
       "address": "Direccion de escuela 3",
       "marker": null
+    }, {
+      "id": "escuela4",
+      "name": "Edlio School",
+      "state": "california",
+      "position": new google.maps.LatLng(33.9825447, -118.4289854),
+      "address": "12910 Culver Blvd, Los Angeles, CA 90066, USA",
+      "marker": null
+    }, {
+      "id": "escuela5",
+      "name": "Rockefeller School",
+      "state": "newyork",
+      "position": new google.maps.LatLng(40.7578981, -73.9863628),
+      "address": "45 Rockefeller Plaza, New York, NY 10111, USA",
+      "marker": null
+    }, {
+      "id": "escuela6",
+      "name": "CN School",
+      "state": "ontario",
+      "position": new google.maps.LatLng(43.6425657, -79.3892444),
+      "address": "301 Front St W",
+      "marker": null
+    }, {
+      "id": "escuela7",
+      "name": "Vancity School",
+      "state": "britishc",
+      "position": new google.maps.LatLng(49.2828286, -123.1392833),
+      "address": "666 East Blvd",
+      "marker": null
     }
   ]
 
   // Create the map
   var map = new google.maps.Map(document.getElementById("map_div"), {
-    center: new google.maps.LatLng(19.4165072, -99.1234034),
-    zoom: 12
+    center: new google.maps.LatLng(41.4565561,-118.4937502),
+    zoom: 4
   })
 
   // Create info windows which will be used by markers
@@ -67,10 +133,28 @@ function initMap() {
     }, "<h1>" + school.name + "</h1><p>" + school.address + "</p>")
   })
 
-  // Function that load the school's select according to a given state
+  // Function that loads the states select according to a given country
+  function filterStates(selectedCountry) {
+    $('#stateSelect').find('option').remove().end()
+      .append('<option selected disabled>Selecciona un estado</option>')
+
+    $.each(states, function(index, state) {
+      if(state.country == selectedCountry) {
+        $("#stateSelect").append('<option value="' +
+          state.id + '">' + state.name + '</option>')
+      } else {
+        $("#stateSelect option[value='" + state.id + "']").remove()
+      }
+    })
+
+    $('#schoolSelect').find('option').remove().end()
+      .append('<option selected disabled>Selecciona una escuela</option>')
+  }
+
+  // Function that loads the school's select according to a given state
   function filterSchools(selectedState) {
     $('#schoolSelect').find('option').remove().end()
-      .append('<option value="">Selecciona una escuela</option>')
+      .append('<option selected disabled>Selecciona una escuela</option>')
 
     $.each(schools, function(index, school) {
       if(school.state == selectedState) {
@@ -78,6 +162,15 @@ function initMap() {
           school.id + '">' + school.name + '</option>')
       } else {
         $("#schoolSelect option[value='" + school.id + "']").remove()
+      }
+    })
+  }
+
+  function moveMapToCountry(countryId) {
+    $.each(countries, function(index, country) {
+      if (country.id == countryId) {
+        map.panTo(country.position)
+        map.setZoom(5)
       }
     })
   }
@@ -101,19 +194,30 @@ function initMap() {
     })
   }
 
+  // Handler for country select change
+  $("#countrySelect").change(function(){
+    var option = $(this).val()
+    switch(option) {
+      case 'us':
+        filterStates(option)
+        moveMapToCountry(option)
+        break
+      case 'mx':
+        filterStates(option)
+        moveMapToCountry(option)
+        break
+      case 'ca':
+        filterStates(option)
+        moveMapToCountry(option)
+        break
+    }
+  })
+
   // Handler for state select change
   $("#stateSelect").change(function(){
   	var option = $(this).val()
-    switch(option) {
-    	case 'cdmx':
-        filterSchools(option)
-        moveMapToState(option)
-        break
-      case 'edomex':
-        filterSchools(option)
-        moveMapToState(option)
-        break
-    }
+    filterSchools(option)
+    moveMapToState(option)
   })
 
   // Handler for school select change
